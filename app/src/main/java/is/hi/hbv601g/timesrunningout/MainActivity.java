a192def840dcaaf4c1a12fea888d1d6127bbc084
Icc2 <56051283+Icc2@users.noreply.github.com>
2022-03-13 16:17:49 +0000

    changes

--- app/src/main/java/is/hi/hbv601g/timesrunningout/MainActivity.java
+++ app/src/main/java/is/hi/hbv601g/timesrunningout/MainActivity.java
@@ -3,12 +3,27 @@ package is.hi.hbv601g.timesrunningout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private Button mButtonPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mButtonPlay = (Button) findViewById(R.id.buttonPlay);
+        mButtonPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: IMPLEMENT THIS
+            }
+        });
     }
 }
